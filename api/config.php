b576b6c482372d5a376ea24c1746423f18eb16ca
kinoppol <kinoppol@hotmail.com>
2026-06-15 21:21:13 +0700

    fix: set default timezone to Asia/Bangkok

--- api/config.php
+++ api/config.php
@@ -3,6 +3,8 @@
 error_reporting(0);
 ini_set('display_errors', '0');
 
+date_default_timezone_set('Asia/Bangkok');
+
 define('DB_HOST',    'localhost');
 define('DB_NAME',    'vec_dve');
 define('DB_USER',    'root');
